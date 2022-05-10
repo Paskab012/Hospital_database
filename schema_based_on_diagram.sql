53d1b8b929cd380bd7ce8f4760840912f58d03a7
vikipretium <vighneshpasupathy@gmail.com>
2022-05-10 23:36:37 +0530

    add invoice table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -20,3 +20,25 @@ CREATE TABLE patients (
 	FOREIGN KEY (patient_id) REFERENCES patients(id),
 	PRIMARY KEY(id)
 );
+
+CREATE TABLE invoices(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    total_amount DECIMAL,
+    generated_at TIMESTAMP, 
+    payed_at TIMESTAMP, 
+    medical_history_id INT,
+    FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id),
+    PRIMARY KEY(id)
+    );
+
+CREATE TABLE invoice_items (
+     id INT GENERATED ALWAYS AS IDENTITY,
+     unit_price DECIMAL,
+     quantity INT,
+     total_price DECIMAL,
+     invoice_id INT,
+     treatment_id INT,
+     FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+     FOREIGN KEY (treatment_id) REFERENCES treatments(id),
+     PRIMARY KEY (id)
+);
