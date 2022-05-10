06b090d0b4c22dd7ae90dac286f1faded5ecfd0e
vikipretium <vighneshpasupathy@gmail.com>
2022-05-10 23:38:39 +0530

    add medical-histories-treatments table M to M

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -43,5 +43,11 @@ CREATE TABLE invoice_items (
      PRIMARY KEY (id)
 );
 
-
+CREATE TABLE medical_histories_treatments (
+medical_history_id INT,
+treatment_id INT,
+FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+FOREIGN KEY (treatment_id) REFERENCES treatments(id),
+PRIMARY KEY (medical_history_id,treatment_id)
+); 
 
