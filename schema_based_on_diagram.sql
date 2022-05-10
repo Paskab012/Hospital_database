e25475016e06cd96afe515da6e6165e7fd69df14
vikipretium <vighneshpasupathy@gmail.com>
2022-05-10 23:37:23 +0530

    add invoice items

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -31,5 +31,17 @@ CREATE TABLE invoices(
     PRIMARY KEY(id)
     );
 
+CREATE TABLE invoice_items (
+     id INT GENERATED ALWAYS AS IDENTITY,
+     unit_price DECIMAL,
+     quantity INT,
+     total_price DECIMAL,
+     invoice_id INT,
+     treatment_id INT,
+     FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+     FOREIGN KEY (treatment_id) REFERENCES treatments(id),
+     PRIMARY KEY (id)
+);
+
 
 
